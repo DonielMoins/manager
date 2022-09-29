8fa36ee5bc92b9fe77bcc5af0765f83e98391f09
Anoop <anooparveti@gmail.com>
2022-09-29 15:44:44 +0530

    fix(pci.object-storage): display errors in regions
     Display the regions which are in error in Container listing page
     ref: MANAGER-9662
    
    Signed-off-by: Anoop <anooparveti@gmail.com>

--- packages/manager/modules/pci/src/projects/project/storages/object-storage/object-storage.routing.js
+++ packages/manager/modules/pci/src/projects/project/storages/object-storage/object-storage.routing.js
@@ -10,9 +10,9 @@ export default /* @ngInject */ ($stateProvider) => {
     redirectTo: (transition) =>
       transition
         .injector()
-        .getAsync('containers')
-        .then((containers) =>
-          containers.length === 0
+        .getAsync('containersResponseObj')
+        .then(({ resources, errors }) =>
+          resources.length === 0 && errors.length === 0
             ? {
                 state:
                   'pci.projects.project.storages.object-storage.onboarding',
@@ -24,12 +24,20 @@ export default /* @ngInject */ ($stateProvider) => {
 
       trackingPrefix: () =>
         'PublicCloud::pci::projects::project::storages::objects::',
-
-      containers: /* @ngInject */ (
+      containersResponseObj: /* @ngInject */ (
         PciProjectStorageContainersService,
         archive,
         projectId,
-      ) => PciProjectStorageContainersService.getAll(projectId, archive),
+      ) =>
+        PciProjectStorageContainersService.getAll(
+          projectId,
+          archive,
+          false,
+          true,
+        ),
+
+      containers: /* @ngInject */ (containersResponseObj) =>
+        containersResponseObj.resources,
 
       containersRegions: /* @ngInject */ (containers) =>
         Array.from(new Set(containers.map(({ region }) => region))),
